b5d1731b3f8538835fc8737777b4e86b98452347
Nick Banken <Nick.Banken@gmail.com>
2019-12-04 16:53:13 +0100

    added the skills card

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const parallax = document.getElementById("parallax1");
+// const parallax = document.getElementById("parallax1");
 
-console.log(parallax);
+// console.log(parallax);
 
-const mq = window.matchMedia( "(max-width: 1000px)" );
-
-
+// const mq = window.matchMedia( "(max-width: 1000px)" );
 
 
-window.addEventListener("scroll",function(){
-    let offset = window.pageYOffset;
-    parallax.style.backgroundPositionY = (offset*-.16)+400 +"px";
-})
+
+
+// window.addEventListener("scroll",function(){
+//     let offset = window.pageYOffset;
+//     parallax.style.backgroundPositionY = (offset*-.06) +"px";
+// })
