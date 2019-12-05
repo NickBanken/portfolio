14f182ec4ae7b34e082b4c35b374b4cfaaaaf96b
nick <nick.banken@gmail.com>
2019-12-05 18:35:01 +0100

    added fix to containers

--- script.js
+++ script.js
@@ -2,22 +2,32 @@ let skills = [
     {
         name:"vuejs",
         color:"green",
-        score:90
+        score:90,
+        skill:"VUE.js",
+        para:`I love to use SASS, it keeps your files structured and readable for others.
+        This combined with the B.E.M method is heaven.
+        <br>
+        Ofcourse, I could drop B.E.M if you'd like!`
     },
     {
         name:"php",
         color:"blue",
-        score:65
+        score:65,
+        skill:"php",
+        para:`I love to use SASS, it keeps your files structured and readable for others.
+        This combined with the B.E.M method is heaven.
+        <br>
+        Ofcourse, I could drop B.E.M if you'd like!`
     }
 ]
 
 for(let i = 0; i < skills.length; i++){
-    createCards(skills[i].name,skills[i].color,skills[i].score)
+    createCards(skills[i].name,skills[i].color,skills[i].score,skills[i].skill,skills[i].para)
 }
 
 
 
-function createCards(skill,color,score){
+function createCards(name,color,score,skill,paragr){
     let row = document.getElementById('skillCards');
     
     //create outer for the card
@@ -26,11 +36,11 @@ function createCards(skill,color,score){
 
      //create outer for the card
      let inner = document.createElement('div');
-     inner.className ="card__inner"
+     inner.classList ="card__inner"
     
     //create the card
     let card = document.createElement('div');
-    card.className = "card__skills card__skills--"+skill;
+    card.className = "card__skills card__skills--"+name;
 
     //create the card container for the image
     let cardCon = document.createElement('div');
@@ -39,7 +49,7 @@ function createCards(skill,color,score){
     //create an svg for the circle
     let svg = document.createElementNS("http://www.w3.org/2000/svg",'svg');
     svg.setAttribute("viewBox", "0 0 36 36");
-    svg.classList = 'circular-chart circular-chart--tomato';
+    svg.classList = 'circular-chart circular-chart--'+color;
 
     //backpath for svg
     let firstPath = document.createElementNS("http://www.w3.org/2000/svg",'path');
@@ -50,7 +60,7 @@ function createCards(skill,color,score){
 
     //frontpath for svg
     let secondPath = document.createElementNS("http://www.w3.org/2000/svg",'path');
-    secondPath.setAttribute("stroke-dasharray","80,100");
+    secondPath.setAttribute("stroke-dasharray",score+",100");
     secondPath.setAttribute("d","M18 2.0845 "+
     "a 15.9155 15.9155 0 0 1 0 31.831 "+
     "a 15.9155 15.9155 0 0 1 0 -31.831");
@@ -58,7 +68,19 @@ function createCards(skill,color,score){
 
     //skill icon
     let icon = document.createElement('i');
-    icon.className = 'devicon-'+skill+'-plain colored devicon';
+    icon.className = 'devicon-'+name+'-plain colored devicon';
+
+
+    //title
+    let title = document.createElement('h3');
+    title.innerHTML = skill;
+
+    //para
+    let para = document.createElement('p');
+    para.innerHTML = paragr;
+    para.classList = "skills__para skills__para--"+name;
+
+
 
     row.appendChild(outer);
     outer.appendChild(inner);
@@ -68,6 +90,8 @@ function createCards(skill,color,score){
     cardCon.appendChild(icon);
     svg.appendChild(firstPath);
     svg.appendChild(secondPath);
+    card.appendChild(title);
+    card.appendChild(para);
     
     
     console.log(row);
